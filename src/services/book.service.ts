1141b5b0dd830f836e3b9c8c1565899a92c27a48
Mochfahmi17 <mochammadfahmiks@gmail.com>
2025-11-17 21:46:07 +0700

    change total to totalBooks at allBooks service

--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -92,16 +92,16 @@ export const allBooks = async ({ categorySlug, search, page = 1, limit = 10 }: a
     pipeline.push({
       $facet: {
         data: [{ $sort: { createdAt: -1 } }, { $skip: skip }, { $limit: limit }],
-        totalCount: [{ $count: "count" }],
+        totalCount: [{ $count: "totalBooks" }],
       },
     });
 
     const result = await Book.aggregate(pipeline);
 
     const data = result[0]?.data ?? [];
-    const total = result[0].totalCount?.[0]?.count ?? 0;
+    const totalBooks = result[0].totalCount?.[0]?.totalBooks ?? 0;
 
-    return { data, pagination: { total, page, limit, totalPages: Math.ceil(total / limit) } };
+    return { data, pagination: { totalBooks, page, limit, totalPages: Math.ceil(totalBooks / limit) } };
   } catch (error) {
     throw error;
   }
